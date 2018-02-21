7fc9d6f26a14a570961eef30ce27887195c3f1f2
cmilkaitis <chris.milkaitis@gmail.com>
2018-02-21 11:34:49 -0500

    self ref1

---
+++ selfref.js
@@ -0,0 +1,11 @@
+'use strict';
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`
+    }
+  };
+  return person;
+}
